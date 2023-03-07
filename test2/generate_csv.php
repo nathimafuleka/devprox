c7ceea84e759634f39bc4207c4c3ca2ca57b687c
Nkosinathi Mafuleka <107852753+nathimafuleka@users.noreply.github.com>
2023-03-07 16:01:50 +0200

    Update generate_csv.php

--- test2/generate_csv.php
+++ test2/generate_csv.php
@@ -1,34 +1,46 @@
+
 <?php
-// Create arrays of names and surnames
+// Create two arrays
 $names = array("Andre", "Tyron", "James", "Hall", "Rob", "Kate", "John", "Mike", "Bob", "Sandy", "Chris", "Sam", "Amy", "Alex", "Liz", "Michael", "Dave", "Rick", "Julie", "Karen"); 
 $surnames = array("van Zuydam", "Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Rodriguez", "Wilson", "Martinez", "Anderson", "Taylor", "Thomas", "Hernandez", "Moore", "Martin", "Jackson", "Thompson"); 
+
 
 function createCSV($num) {
   global $names;
   global $surnames;
 
-  // Create the CSV file and write the header
-  $file = fopen("output.csv", "w");
-  fwrite($file, "Id,Name,Surname,Initials,Age,DateOfBirth\n");
-
-  // Generate the records and write them to the file
-  for ($i = 0; $i < $num; $i++) {
+  $csv = "Id,Name,Surname,Initials,Age,DateOfBirth\n";
+  
+  $records = array();
+  $i = 0;
+  
+  while ($i < $num) {
     // Generate random name, age & birthdate
-    $name = $names[array_rand($names)];
-    $surname = $surnames[array_rand($surnames)];
+    $name = $names[rand(0, 19)];
+    $surname = $surnames[rand(0, 19)];
     $initials = substr($name, 0, 1);
     $age = rand(18, 65);
     $date = date("d/m/Y", strtotime("-$age years"));
 
-    // Write the record to the file
-    fwrite($file, "\"$i\",\"$name\",\"$surname\",\"$initials\",\"$age\",\"$date\"\n");
+    // Check if record already exists
+    $record = "$name $surname $age $date";
+    if (!in_array($record, $records)) {
+      // Add record to array
+      array_push($records, $record);
+      // Add row to csv
+      $csv .= "\"$i\",\"$name\",\"$surname\",\"$initials\",\"$age\",\"$date\"\n";
+      $i++;
+    }
   }
   
-  // Close the file
+  // Output csv
+  $file = fopen("output.csv", "w");
+  fwrite($file, $csv);
   fclose($file);
 
   echo "File generated";
 }
+
 
 if(isset($_POST['num_records'])) {
   $num_records = $_POST['num_records'];
